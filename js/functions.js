50f1ffa572be974a7b7b50bbfc28962cb5902bfd
raul <raul@zamacona.me>
2014-08-01 18:14:54 -0500

    home y categorias

--- js/functions.js
+++ js/functions.js
@@ -7,27 +7,44 @@
 		/**
 		 * Validación de emails
 		 */
-		window.validateEmail = function (email) {
-			var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-			return regExp.test(email);
-		};
-
-
+		// window.validateEmail = function (email) {
+		// 	var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		// 	return regExp.test(email);
+		// };
 
 		/**
 		 * Regresa todos los valores de un formulario como un associative array
 		 */
-		window.getFormData = function (selector) {
-			var result = [],
-				data   = $(selector).serializeArray();
+		// window.getFormData = function (selector) {
+		// 	var result = [],
+		// 		data   = $(selector).serializeArray();
 
-			$.map(data, function (attr) {
-				result[attr.name] = attr.value;
-			});
-			return result;
-		}
+		// 	$.map(data, function (attr) {
+		// 		result[attr.name] = attr.value;
+		// 	});
+		// 	return result;
+		// }
 
+		$('.menu').on('click', function(){
+			abrirMenu( $(this), $('nav') );
+		});
+
+		$('.tabs a').on('click', function(){
+			cambiarTab( $(this) );
+		});
 
 	});
+
+	function abrirMenu(elemento, menu){
+		menu.slideToggle('fast');
+	}
+
+	function cambiarTab(elemento){
+		var tabAAbrir = elemento.data('tab');
+		$('.tabs a').removeClass('active');
+		elemento.addClass('active');
+		$('.tab-content').addClass('hide');
+		$('.'+tabAAbrir).removeClass('hide');
+	}
 
 })(jQuery);
