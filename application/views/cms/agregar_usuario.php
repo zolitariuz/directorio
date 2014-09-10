13fc09b26cb28ed3d6ea459c7c7793731930d905
raul <raul@zamacona.me>
2014-09-10 01:52:22 -0500

    agregar usuario

--- application/views/cms/agregar_usuario.php
+++ application/views/cms/agregar_usuario.php
@@ -1,46 +1,45 @@
 <?php if(isset($usuario)) { ?>
 
 <div class="main">
-	<a href="<?php echo base_url().'index.php/gestor_contenidos/panel_admin/' ?>" class="margin-bottom">Regresar a panel de administración</a>
 	<div class="width">
-		<div class="full">
-			<?php 
+		<div class="columna xmall-6 center">
+			<?php
 				if(isset($success)){
 					echo '<p class="success">'.$success.'</p>';
 				} else if(isset($error)){
 					echo '<p class="error">'.$error.'</p>';
 				}
 			?>
-			<form method="POST" action="<?php echo base_url().'index.php/gestor_contenidos/agregar_usuario' ?>" class="columna xmall-12 center crea-aviso">
-				<fieldset class="columna xmall-6">
+			<form class="crea-aviso js-validate" method="POST" action="<?php echo base_url().'index.php/gestor_contenidos/agregar_usuario' ?>">
+				<fieldset class="">
 					<label for="usuario" >Nombre de usuario</label>
-					<input type="text" name="usuario" class="columna xmall-12">
+					<input type="text" name="usuario" class="full">
 				</fieldset>
-				<fieldset class="columna xmall-6">
+				<fieldset class="">
 					<label for="password" >Contraseña</label>
-					<input type="text" name="password" class="columna xmall-12">
+					<input type="text" name="password" class="full">
 				</fieldset>
-				<fieldset class="columna xmall-6">
+				<fieldset class="">
 					<label for="nombre" >Nombre</label>
-					<input type="text" name="nombre" class="columna xmall-12">
+					<input type="text" name="nombre" class="full">
 				</fieldset>
-				<fieldset class="columna xmall-6">
+				<fieldset class="">
 					<label for="apellidos" >Apellidos</label>
-					<input type="text" name="apellidos" class="columna xmall-12">
+					<input type="text" name="apellidos" class="full">
 				</fieldset>
-				<fieldset class="columna xmall-6 url_aviso"> 
+				<fieldset class=" url_aviso">
 					<label for="nombre" >Tipo de usuario</label>
-					<select name="rol" class="columna xmall-12">
+					<select name="rol" class="full">
 						<option value="admin">Administrador</option>
 						<option value="editor">Editor</option>
 					</select>
 				</fieldset>
-				<fieldset class="columna xmall-10 center"> 
+				<fieldset class="">
 					<input type="hidden" name="id_usuario" value="<?php echo $usuario['id_usuario'] ?>">
-					<input type="submit" value="Agregar usuario" class="columna xmall-3 block center boton">
+					<button type="submit" class="block boton chico right">Agregar usuario</button>
 				</fieldset>
 			</form>
-		</div>
+		</div><!-- columna xmall-6 center -->
 	</div>
 </div>
 
