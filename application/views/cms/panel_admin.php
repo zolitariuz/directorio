c6d4b3fcc5be08c534f594347930fd6319b7f3d7
raul <raul@zamacona.me>
2014-09-17 16:02:01 -0500

    responsive

--- application/views/cms/panel_admin.php
+++ application/views/cms/panel_admin.php
@@ -7,27 +7,27 @@
 			<?php if($_SESSION['is_admin'] == 't') { ?>
 			<a href="<?php echo base_url().'index.php/gestor_contenidos/agregar_contenido/'?>" class="block boton vertical columna xmall-6 margin-bottom">
 				<i class="icon-ts-icon-line-agregar-contenido"></i>
-				<!-- <i class="icon-ts-icon-fill-agregar-contenido"></i> -->
+				<!-- <i class="icon-ts-icon-filled-agregar-contenido"></i> -->
 				Agregar contenido
 			</a>
 			<a href="<?php echo base_url().'index.php/gestor_contenidos/agregar_usuario/'?>" class="block boton vertical columna xmall-6 margin-bottom">
-				<i class="icon-ts-icon-fill-agregar-usuario"></i>
+				<i class="icon-ts-icon-filled-agregar-usuario"></i>
 				Agregar usuario
 			</a>
 			<?php } ?>
 			<div class="clear"></div>
 			<a href="<?php echo base_url().'index.php/gestor_contenidos/editar_contenido/'?>" class="block boton vertical columna xmall-6 margin-bottom">
-				<i class="icon-ts-icon-fill-editar-contenido"></i>
+				<i class="icon-ts-icon-filled-editar-contenido"></i>
 				Editar contenido
 			</a>
 			<a href="<?php echo base_url().'index.php/gestor_contenidos/mas_solicitados/'?>" class="block boton vertical columna xmall-6 margin-bottom">
-				<i class="icon-ts-icon-fill-tramites-y-servicios-mas-solicitados"></i>
+				<i class="icon-ts-icon-filled-tramites-y-servicios-mas-solicitados"></i>
 				Trámites y servicios mas solicitados
 			</a>
 			<div class="clear"></div>
 			<a href="<?php echo base_url().'index.php/gestor_contenidos/panel_reportes/'?>" class="block boton vertical columna xmall-6 margin-bottom">
-				<i class="icon-ts-icon-fill-reportes"></i>
-				Reportes de trámites/servicios 
+				<i class="icon-ts-icon-filled-reportes"></i>
+				Reportes de trámites/servicios
 			</a>
 		</div>
 	</div>
