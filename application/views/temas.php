21770af958222bac5e76af85ce65d642ab789dd9
raul <raul@zamacona.me>
2014-09-18 15:38:44 -0500

    correciones margenes temas e insituciones

--- application/views/temas.php
+++ application/views/temas.php
@@ -13,7 +13,7 @@
 				$tema = $value->materia;
 				$idMateria = $value->id_cat_materia;
 			?>
-				<a href="<?php echo base_url().'index.php/temas/muestraTS/'.$idMateria; ?>" class="boton columna xmall-12 small-6 medium-4 large-3 xlarge-2 cuadrado chico">
+				<a href="<?php echo base_url().'index.php/temas/muestraTS/'.$idMateria; ?>" class="boton columna xmall-12 xmall-6 medium-4 large-3 xlarge-2 cuadrado chico">
 					<i class="fa fa-asterisk"></i>
 					<?php echo $tema; ?>
 				</a>
