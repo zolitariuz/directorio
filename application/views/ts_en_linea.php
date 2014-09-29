a67ad276b431cc7f2bc73471c77980c92655621e
maichomper <maichomper@gmail.com>
2014-09-29 16:55:41 -0500

    tramite y servicio en linea con breadcrumbs

--- application/views/ts_en_linea.php
+++ application/views/ts_en_linea.php
@@ -5,7 +5,7 @@
 		<ul class="breadcrumbs">
 			<li><a href="<?php echo base_url() ?>"><i class="fa fa-home"></i>Inicio</a></li>
 			<li>></li>
-			<li class="actual">Instituciones</li>
+			<li class="actual">Trámites y servicios en línea</li>
 		</ul>
 		<section class="mas-comunes clearfix">
 			<h2 class="text-center highlight">Trámites y servicios en linea</h2>
